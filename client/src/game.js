1b527b2db9d4b2830ee69cbe7b18ecddc4306aea
Maddie Dawson <maddie.d.11@gmail.com>
2020-06-30 11:19:49 -0700

    Revert "Fix dulpicate cards"
    This reverts commit 8d44ccd582028ef50023de335c3fe066758c18a8.

--- client/src/game.js
+++ client/src/game.js
@@ -1,72 +1,72 @@
 function setCardImages() {
   // Threes.
-  $('.1-1').attr('src', 'https://upload.wikimedia.org/wikipedia/commons/3/32/Poker-sm-24C-3c.png');
-  $('.1-2').attr('src', 'https://upload.wikimedia.org/wikipedia/commons/d/d4/Poker-sm-23C-3d.png');
-  $('.1-3').attr('src', 'https://upload.wikimedia.org/wikipedia/commons/8/80/Poker-sm-22C-3h.png');
-  $('.1-4').attr('src', 'https://upload.wikimedia.org/wikipedia/commons/2/2c/Poker-sm-21C-3s.png');
+  $('#1-1').attr('src', 'https://upload.wikimedia.org/wikipedia/commons/3/32/Poker-sm-24C-3c.png');
+  $('#1-2').attr('src', 'https://upload.wikimedia.org/wikipedia/commons/d/d4/Poker-sm-23C-3d.png');
+  $('#1-3').attr('src', 'https://upload.wikimedia.org/wikipedia/commons/8/80/Poker-sm-22C-3h.png');
+  $('#1-4').attr('src', 'https://upload.wikimedia.org/wikipedia/commons/2/2c/Poker-sm-21C-3s.png');
   // Fours.
-  $('.2-1').attr('src', 'https://upload.wikimedia.org/wikipedia/commons/9/90/Poker-sm-24B-4c.png');
-  $('.2-2').attr('src', 'https://upload.wikimedia.org/wikipedia/commons/a/ab/Poker-sm-23B-4d.png');
-  $('.2-3').attr('src', 'https://upload.wikimedia.org/wikipedia/commons/7/7a/Poker-sm-22B-4h.png');
-  $('.2-4').attr('src', 'https://upload.wikimedia.org/wikipedia/commons/4/4b/Poker-sm-21B-4s.png');
+  $('#2-1').attr('src', 'https://upload.wikimedia.org/wikipedia/commons/9/90/Poker-sm-24B-4c.png');
+  $('#2-2').attr('src', 'https://upload.wikimedia.org/wikipedia/commons/a/ab/Poker-sm-23B-4d.png');
+  $('#2-3').attr('src', 'https://upload.wikimedia.org/wikipedia/commons/7/7a/Poker-sm-22B-4h.png');
+  $('#2-4').attr('src', 'https://upload.wikimedia.org/wikipedia/commons/4/4b/Poker-sm-21B-4s.png');
   // Fives.
-  $('.3-1').attr('src', 'https://upload.wikimedia.org/wikipedia/commons/4/48/Poker-sm-24A-5c.png');
-  $('.3-2').attr('src', 'https://upload.wikimedia.org/wikipedia/commons/4/48/Poker-sm-23A-5d.png');
-  $('.3-3').attr('src', 'https://upload.wikimedia.org/wikipedia/commons/d/d3/Poker-sm-22A-5h.png');
-  $('.3-4').attr('src', 'https://upload.wikimedia.org/wikipedia/commons/8/86/Poker-sm-21A-5s.png');
+  $('#3-1').attr('src', 'https://upload.wikimedia.org/wikipedia/commons/4/48/Poker-sm-24A-5c.png');
+  $('#3-2').attr('src', 'https://upload.wikimedia.org/wikipedia/commons/4/48/Poker-sm-23A-5d.png');
+  $('#3-3').attr('src', 'https://upload.wikimedia.org/wikipedia/commons/d/d3/Poker-sm-22A-5h.png');
+  $('#3-4').attr('src', 'https://upload.wikimedia.org/wikipedia/commons/8/86/Poker-sm-21A-5s.png');
   // Sixes.
-  $('.4-1').attr('src', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Poker-sm-249-6c.png');
-  $('.4-2').attr('src', 'https://upload.wikimedia.org/wikipedia/commons/7/73/Poker-sm-239-6d.png');
-  $('.4-3').attr('src', 'https://upload.wikimedia.org/wikipedia/commons/7/72/Poker-sm-229-6h.png');
-  $('.4-4').attr('src', 'https://upload.wikimedia.org/wikipedia/commons/0/05/Poker-sm-219-6s.png');
+  $('#4-1').attr('src', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Poker-sm-249-6c.png');
+  $('#4-2').attr('src', 'https://upload.wikimedia.org/wikipedia/commons/7/73/Poker-sm-239-6d.png');
+  $('#4-3').attr('src', 'https://upload.wikimedia.org/wikipedia/commons/7/72/Poker-sm-229-6h.png');
+  $('#4-4').attr('src', 'https://upload.wikimedia.org/wikipedia/commons/0/05/Poker-sm-219-6s.png');
   // Sevens.
-  $('.5-1').attr('src', 'https://upload.wikimedia.org/wikipedia/commons/b/ba/Poker-sm-248-7c.png');
-  $('.5-2').attr('src', 'https://upload.wikimedia.org/wikipedia/commons/c/cd/Poker-sm-238-7d.png');
-  $('.5-3').attr('src', 'https://upload.wikimedia.org/wikipedia/commons/4/43/Poker-sm-228-7h.png');
-  $('.5-4').attr('src', 'https://upload.wikimedia.org/wikipedia/commons/e/e2/Poker-sm-218-7s.png');
+  $('#5-1').attr('src', 'https://upload.wikimedia.org/wikipedia/commons/b/ba/Poker-sm-248-7c.png');
+  $('#5-2').attr('src', 'https://upload.wikimedia.org/wikipedia/commons/c/cd/Poker-sm-238-7d.png');
+  $('#5-3').attr('src', 'https://upload.wikimedia.org/wikipedia/commons/4/43/Poker-sm-228-7h.png');
+  $('#5-4').attr('src', 'https://upload.wikimedia.org/wikipedia/commons/e/e2/Poker-sm-218-7s.png');
   // Eights.
-  $('.6-1').attr('src', 'https://upload.wikimedia.org/wikipedia/commons/6/6e/Poker-sm-247-8c.png');
-  $('.6-2').attr('src', 'https://upload.wikimedia.org/wikipedia/commons/c/c6/Poker-sm-237-8d.png');
-  $('.6-3').attr('src', 'https://upload.wikimedia.org/wikipedia/commons/a/a9/Poker-sm-227-8h.png');
-  $('.6-4').attr('src', 'https://upload.wikimedia.org/wikipedia/commons/e/e6/Poker-sm-217-8s.png');
+  $('#6-1').attr('src', 'https://upload.wikimedia.org/wikipedia/commons/6/6e/Poker-sm-247-8c.png');
+  $('#6-2').attr('src', 'https://upload.wikimedia.org/wikipedia/commons/c/c6/Poker-sm-237-8d.png');
+  $('#6-3').attr('src', 'https://upload.wikimedia.org/wikipedia/commons/a/a9/Poker-sm-227-8h.png');
+  $('#6-4').attr('src', 'https://upload.wikimedia.org/wikipedia/commons/e/e6/Poker-sm-217-8s.png');
   // Nines.
-  $('.7-1').attr('src', 'https://upload.wikimedia.org/wikipedia/commons/6/64/Poker-sm-246-9c.png');
-  $('.7-2').attr('src', 'https://upload.wikimedia.org/wikipedia/commons/c/c0/Poker-sm-236-9d.png');
-  $('.7-3').attr('src', 'https://upload.wikimedia.org/wikipedia/commons/d/d6/Poker-sm-226-9h.png');
-  $('.7-4').attr('src', 'https://upload.wikimedia.org/wikipedia/commons/3/30/Poker-sm-216-9s.png');
+  $('#7-1').attr('src', 'https://upload.wikimedia.org/wikipedia/commons/6/64/Poker-sm-246-9c.png');
+  $('#7-2').attr('src', 'https://upload.wikimedia.org/wikipedia/commons/c/c0/Poker-sm-236-9d.png');
+  $('#7-3').attr('src', 'https://upload.wikimedia.org/wikipedia/commons/d/d6/Poker-sm-226-9h.png');
+  $('#7-4').attr('src', 'https://upload.wikimedia.org/wikipedia/commons/3/30/Poker-sm-216-9s.png');
   // Tens.
-  $('.8-1').attr('src', 'https://upload.wikimedia.org/wikipedia/commons/f/f3/Poker-sm-245-Tc.png');
-  $('.8-2').attr('src', 'https://upload.wikimedia.org/wikipedia/commons/6/6d/Poker-sm-235-Td.png');
-  $('.8-3').attr('src', 'https://upload.wikimedia.org/wikipedia/commons/f/fe/Poker-sm-225-Th.png');
-  $('.8-4').attr('src', 'https://upload.wikimedia.org/wikipedia/commons/5/50/Poker-sm-215-Ts.png');
+  $('#8-1').attr('src', 'https://upload.wikimedia.org/wikipedia/commons/f/f3/Poker-sm-245-Tc.png');
+  $('#8-2').attr('src', 'https://upload.wikimedia.org/wikipedia/commons/6/6d/Poker-sm-235-Td.png');
+  $('#8-3').attr('src', 'https://upload.wikimedia.org/wikipedia/commons/f/fe/Poker-sm-225-Th.png');
+  $('#8-4').attr('src', 'https://upload.wikimedia.org/wikipedia/commons/5/50/Poker-sm-215-Ts.png');
   // Jacks.
-  $('.9-1').attr('src', 'https://upload.wikimedia.org/wikipedia/commons/2/2f/Poker-sm-244-Jc.png');
-  $('.9-2').attr('src', 'https://upload.wikimedia.org/wikipedia/commons/a/aa/Poker-sm-234-Jd.png');
-  $('.9-3').attr('src', 'https://upload.wikimedia.org/wikipedia/commons/c/ce/Poker-sm-224-Jh.png');
-  $('.9-4').attr('src', 'https://upload.wikimedia.org/wikipedia/commons/5/5e/Poker-sm-214-Js.png');
+  $('#9-1').attr('src', 'https://upload.wikimedia.org/wikipedia/commons/2/2f/Poker-sm-244-Jc.png');
+  $('#9-2').attr('src', 'https://upload.wikimedia.org/wikipedia/commons/a/aa/Poker-sm-234-Jd.png');
+  $('#9-3').attr('src', 'https://upload.wikimedia.org/wikipedia/commons/c/ce/Poker-sm-224-Jh.png');
+  $('#9-4').attr('src', 'https://upload.wikimedia.org/wikipedia/commons/5/5e/Poker-sm-214-Js.png');
   // Queens.
-  $('.10-1').attr('src', 'https://upload.wikimedia.org/wikipedia/commons/1/1e/Poker-sm-243-Qc.png');
-  $('.10-2').attr('src', 'https://upload.wikimedia.org/wikipedia/commons/7/70/Poker-sm-233-Qd.png');
-  $('.10-3').attr('src', 'https://upload.wikimedia.org/wikipedia/commons/5/5d/Poker-sm-223-Qh.png');
-  $('.10-4').attr('src', 'https://upload.wikimedia.org/wikipedia/commons/a/a5/Poker-sm-213-Qs.png');
+  $('#10-1').attr('src', 'https://upload.wikimedia.org/wikipedia/commons/1/1e/Poker-sm-243-Qc.png');
+  $('#10-2').attr('src', 'https://upload.wikimedia.org/wikipedia/commons/7/70/Poker-sm-233-Qd.png');
+  $('#10-3').attr('src', 'https://upload.wikimedia.org/wikipedia/commons/5/5d/Poker-sm-223-Qh.png');
+  $('#10-4').attr('src', 'https://upload.wikimedia.org/wikipedia/commons/a/a5/Poker-sm-213-Qs.png');
   // Kings.
-  $('.11-1').attr('src', 'https://upload.wikimedia.org/wikipedia/commons/2/25/Poker-sm-242-Kc.png');
-  $('.11-2').attr('src', 'https://upload.wikimedia.org/wikipedia/commons/d/d0/Poker-sm-232-Kd.png');
-  $('.11-3').attr('src', 'https://upload.wikimedia.org/wikipedia/commons/7/79/Poker-sm-222-Kh.png');
-  $('.11-4').attr('src', 'https://upload.wikimedia.org/wikipedia/commons/9/9b/Poker-sm-212-Ks.png');
+  $('#11-1').attr('src', 'https://upload.wikimedia.org/wikipedia/commons/2/25/Poker-sm-242-Kc.png');
+  $('#11-2').attr('src', 'https://upload.wikimedia.org/wikipedia/commons/d/d0/Poker-sm-232-Kd.png');
+  $('#11-3').attr('src', 'https://upload.wikimedia.org/wikipedia/commons/7/79/Poker-sm-222-Kh.png');
+  $('#11-4').attr('src', 'https://upload.wikimedia.org/wikipedia/commons/9/9b/Poker-sm-212-Ks.png');
   // Aces.
-  $('.12-1').attr('src', 'https://upload.wikimedia.org/wikipedia/commons/3/38/Poker-sm-241-Ac.png');
-  $('.12-2').attr('src', 'https://upload.wikimedia.org/wikipedia/commons/b/be/Poker-sm-231-Ad.png');
-  $('.12-3').attr('src', 'https://upload.wikimedia.org/wikipedia/commons/d/d3/Poker-sm-221-Ah.png');
-  $('.12-4').attr('src', 'https://upload.wikimedia.org/wikipedia/commons/3/35/Poker-sm-211-As.png');
+  $('#12-1').attr('src', 'https://upload.wikimedia.org/wikipedia/commons/3/38/Poker-sm-241-Ac.png');
+  $('#12-2').attr('src', 'https://upload.wikimedia.org/wikipedia/commons/b/be/Poker-sm-231-Ad.png');
+  $('#12-3').attr('src', 'https://upload.wikimedia.org/wikipedia/commons/d/d3/Poker-sm-221-Ah.png');
+  $('#12-4').attr('src', 'https://upload.wikimedia.org/wikipedia/commons/3/35/Poker-sm-211-As.png');
   // Twos.
-  $('.13-1').attr('src', 'https://upload.wikimedia.org/wikipedia/commons/c/cd/Poker-sm-24D-2c.png');
-  $('.13-2').attr('src', 'https://upload.wikimedia.org/wikipedia/commons/b/bb/Poker-sm-23D-2d.png');
-  $('.13-3').attr('src', 'https://upload.wikimedia.org/wikipedia/commons/2/20/Poker-sm-22D-2h.png');
-  $('.13-4').attr('src', 'https://upload.wikimedia.org/wikipedia/commons/c/c5/Poker-sm-21D-2s.png');
+  $('#13-1').attr('src', 'https://upload.wikimedia.org/wikipedia/commons/c/cd/Poker-sm-24D-2c.png');
+  $('#13-2').attr('src', 'https://upload.wikimedia.org/wikipedia/commons/b/bb/Poker-sm-23D-2d.png');
+  $('#13-3').attr('src', 'https://upload.wikimedia.org/wikipedia/commons/2/20/Poker-sm-22D-2h.png');
+  $('#13-4').attr('src', 'https://upload.wikimedia.org/wikipedia/commons/c/c5/Poker-sm-21D-2s.png');
   // Jokers.
-  $('.14-').attr('src', 'https://upload.wikimedia.org/wikipedia/commons/2/23/The_Jolly_Nero.jpg');
-  $('.15-').attr('src', 'https://upload.wikimedia.org/wikipedia/commons/d/db/The_Jolly_Rosso.jpg');
+  $('#14-').attr('src', 'https://upload.wikimedia.org/wikipedia/commons/2/23/The_Jolly_Nero.jpg');
+  $('#15-').attr('src', 'https://upload.wikimedia.org/wikipedia/commons/d/db/The_Jolly_Rosso.jpg');
 }
 
 function getDifferenceBetweenVisibleAndCardWidths() {
@@ -89,17 +89,7 @@ function recreateClickableCardTops() {
   let numCards = $('.game-card').length;
   let numClickableDivsCreated = 0;
   $('.game-card-img').each(function() {
-    let classList = $(element).attr('class').split(/\s+/);
-    let cardId = '';
-    for (c of classList) {
-      let re = /[0-9]+-[0-9]*/;
-      let match = c.match(re);
-      if (match.length > 0) {
-        cardId = match[0];
-        break;
-      }
-    }
-    let id = cardId + '-clickable';
+    let id = $(this).attr('id') + '-clickable';
     let offset = $(this).offset();
     let lastCard = numClickableDivsCreated === numCards - 1;
     if (lastCard) {
@@ -122,20 +112,11 @@ function recreateClickableCardTops() {
     numClickableDivsCreated++;
   });
   $('.clickable-card-top').click(function() {
-    let classList = $(element).attr('class').split(/\s+/);
-    let id = '';
-    for (c of classList) {
-      let re = /[0-9]+-[0-9]*/;
-      let match = c.match(re);
-      if (match.length > 0) {
-        id = match[0];
-        break;
-      }
-    }
+    let id = $(this).attr('id');
     let i = id.indexOf('-clickable-top');
     let cardId = id.substring(0, i);
-    $('.' + cardId + '-clickable').toggleClass('clickable-card-selected');
-    $('.' + cardId).parent().toggleClass('card-selected');
+    $('#' + cardId + '-clickable').toggleClass('clickable-card-selected');
+    $('#' + cardId).parent().toggleClass('card-selected');
     updateButtons();
     recreateClickableCardTops();
   });
@@ -177,17 +158,7 @@ function overlapCardsAndCreateClickableDivs() {
   }
   let numClickableDivsCreated = 0;
   $('.game-card-img').each(function() {
-    let classList = $(element).attr('class').split(/\s+/);
-    let cardId = '';
-    for (c of classList) {
-      let re = /[0-9]+-[0-9]*/;
-      let match = c.match(re);
-      if (match.length > 0) {
-        cardId = match[0];
-        break;
-      }
-    }
-    let id = cardId + '-clickable';
+    let id = $(this).attr('id') + '-clickable';
     let offset = $(this).offset();
     let height = $(this).height();
     let lastCard = numClickableDivsCreated === numCards - 1;
@@ -209,20 +180,11 @@ function overlapCardsAndCreateClickableDivs() {
   });
   recreateClickableCardTops();
   $('.clickable-card').click(function() {
-    let classList = $(element).attr('class').split(/\s+/);
-    let id = '';
-    for (c of classList) {
-      let re = /[0-9]+-[0-9]*/;
-      let match = c.match(re);
-      if (match.length > 0) {
-        id = match[0];
-        break;
-      }
-    }
+    let id = $(this).attr('id');
     let i = id.indexOf('-clickable');
     let cardId = id.substring(0, i);
     $(this).toggleClass('clickable-card-selected');
-    $('.' + cardId).parent().toggleClass('card-selected');
+    $('#' + cardId).parent().toggleClass('card-selected');
     updateButtons();
     recreateClickableCardTops();
   });
@@ -238,7 +200,7 @@ function resizeCenter() {
 function getSelectedCards() {
   let hand = [];
   $('.game-card.card-selected').each(function() {
-    let valueAndSuit = $(this).children('img').attr('class').split('-');
+    let valueAndSuit = $(this).children('img').attr('id').split('-');
     let value = parseInt(valueAndSuit[0]);
     if (value == 14 || value == 15) {
       hand.push({ value: value });
